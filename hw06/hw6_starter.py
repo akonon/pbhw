11723cb1f5c3c9508b3e0d486443b198a0c2b3e7
Alex Kononenko <konondev@gmail.com>
2016-11-06 11:38:09 +0300

    Copy code to hw6_starter.py

--- hw06/hw6_starter.py
+++ hw06/hw6_starter.py
@@ -1,1 +1,22 @@
+#!/usr/env/bin python
+# -*- coding: utf-8 -*-
 
+u"""
+Основной скрипт запуска ДЗ.
+
+Данный скрипт призван запускать на выполнение домашнее задание #6.
+"""
+
+
+from hw6_solution1 import modifier
+
+
+def runner():
+    u"""Запускает выполнение всех задач"""
+    print "Modifying file..."
+    modifier("data.csv")
+    print "Modified successfully!"
+
+
+if __name__ == '__main__':
+    runner()
